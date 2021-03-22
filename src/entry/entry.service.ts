1d558fe20485cec0b5a91a8fafbfa1454ee7546c
Michał Jung <stackustack@gmail.com>
2021-03-22 09:57:08 +0100

    asd

--- src/entry/entry.service.ts
+++ src/entry/entry.service.ts
@@ -18,10 +18,10 @@ export class EntrysService {
     async add(): Promise<Entry> {
         const newEntry = new Entry();
         const PRICE = ".ty-product-block__price-actual .ty-price-update .ty-price bdi .ty-price-num:first-of-type"
+        const dateStamp = new Date().toISOString()
 
         const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
         const page = await browser.newPage();
-
         await page.goto('https://www.riskmadeinwarsaw.com/en/clothes-category/blouses/pure-bliss-long-sleeve-art-blue');
 
         const price = await page.evaluate((PRICE) => {
@@ -32,7 +32,7 @@ export class EntrysService {
 
         await browser.close();
 
-        newEntry.entry = price.toString()
+        newEntry.entry = `Price at dateStamp: ${price.toString()}`
 
         return this.entryRepository.save(newEntry)
     }
