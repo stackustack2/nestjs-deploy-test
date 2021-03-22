d4d419708ba4535dc6c189caa8331581823b2de9
Michał Jung <stackustack@gmail.com>
2021-03-22 11:20:07 +0100

    Add Cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,7 +10,8 @@ import { EntrysModule } from './entry/entrys.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(),
-    EntrysModule
+    ScheduleModule.forRoot(),
+    EntrysModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/entry/entry.service.ts
+++ src/entry/entry.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as puppeteer from 'puppeteer'
 import { Entry } from './entry.entity';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class EntrysService {
+    private readonly logger = new Logger(EntrysService.name);
+
     constructor(
         @InjectRepository(Entry)
         private entryRepository: Repository<Entry>
@@ -35,5 +38,27 @@ export class EntrysService {
         newEntry.entry = `Price at ${dateStamp}: ${price.toString()}PLN`
 
         return this.entryRepository.save(newEntry)
+    }
+
+    @Cron('*/28 * * * *') // Every 28th minute (also to keep Heroku from idling)
+    async handleCron() {
+
+        const newEntry = new Entry();
+        const PRICE = ".ty-product-block__price-actual .ty-price-update .ty-price bdi .ty-price-num:first-of-type"
+        const dateStamp = new Date().toISOString()
+
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+        const page = await browser.newPage();
+        await page.goto('https://www.riskmadeinwarsaw.com/en/clothes-category/blouses/pure-bliss-long-sleeve-art-blue');
+
+        const price = await page.evaluate((PRICE) => {
+            return document
+                .querySelector(PRICE)
+                .textContent
+        }, PRICE);
+
+        await browser.close();
+
+        newEntry.entry = `[Created by CRON] Price at ${dateStamp}: ${price.toString()}PLN`
     }
 }
